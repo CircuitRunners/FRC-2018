fec3ce2fe33268851d6601f937317fc6dd9bce96
colinmcmillen <jerrym08@me.com>
2018-02-06 17:16:32 -0500

    Formatted Robot.java

--- PowerUpInterative/src/org/usfirst/frc/team1002/robot/Robot.java
+++ PowerUpInterative/src/org/usfirst/frc/team1002/robot/Robot.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -22,48 +21,48 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends IterativeRobot {
-	
+
 	public static XboxController driver = new XboxController(RobotMap.driverPort);
 	static MarioDrive drive;
-	//MyAutonomous auto;
+	// MyAutonomous auto;
 	gcbCameraSample cam;
 	String gameData;
+
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 
 	@Override
 	public void robotInit() {
 		drive = new MarioDrive();
 		cam = new gcbCameraSample();
-		//auto = new MyAutonomous();
+		// auto = new MyAutonomous();
 		drive.gyro.reset();
 		cam.cameraInit();
-		
+
 	}
-	
-	
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString line to get the
+	 * auto name from the text box below the Gyro
 	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
+	 * <p>
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the SendableChooser
+	 * make sure to add them to the chooser code above as well.
 	 */
-	
+
 	@Override
 	public void autonomousInit() {
 		// autoSelected = SmartDashboard.getString("Auto Selector",
 		// defaultAuto);
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		SmartDashboard.putString("Game Data",gameData);
-		
+		SmartDashboard.putString("Game Data", gameData);
+
 	}
 
 	/**
@@ -71,7 +70,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		if(gameData.charAt(0) == 'L') {
+		if (gameData.charAt(0) == 'L') {
 			SmartDashboard.putString("Data Recieved", "Left");
 		}
 	}
@@ -82,21 +81,20 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 
-		if(driver.getAButton()) {
+		if (driver.getAButton()) {
 			drive.autoDrive(0.2, 3.0, 3.0);
-		}else if(driver.getBumper(GenericHID.Hand.kRight)) {
+		} else if (driver.getBumper(GenericHID.Hand.kRight)) {
 			drive.autoDrive(0.2, 3.0, -3.0);
-		}else if(driver.getBumper(GenericHID.Hand.kLeft)) {
+		} else if (driver.getBumper(GenericHID.Hand.kLeft)) {
 			drive.autoTurn(90, 4.0);
-		}else if(driver.getXButton()) {
+		} else if (driver.getXButton()) {
 			drive.autoTurn(0.0, 4.0);
-		}else if(driver.getYButton()) {
+		} else if (driver.getYButton()) {
 			drive.autoDrive(0.3, 10.0, 10.0);
 			drive.autoTurn(90, 5.0);
 			drive.autoDrive(0.2, 6.0, 4.0);
-		}
-		else {
-		drive.teleOp();
+		} else {
+			drive.teleOp();
 		}
 	}
 
@@ -105,15 +103,15 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void testPeriodic() {
-		
+
 	}
+
 	@Override
 	public void disabledPeriodic() {
 		SmartDashboard.putNumber("Left Encoder Count", drive.encL.get());
 		SmartDashboard.putNumber("Left Encoder Distance", drive.encL.getDistance());
 		SmartDashboard.putNumber("Right Encoder Count", drive.encR.get());
-		SmartDashboard.putNumber("Right Encoder Distance", drive.encR.getDistance());	
+		SmartDashboard.putNumber("Right Encoder Distance", drive.encR.getDistance());
 	}
-	
-		
+
 }
