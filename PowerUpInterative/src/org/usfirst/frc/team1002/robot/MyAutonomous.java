619c0ab24787b795bbae1c45b72b1c8fe255f60e
colinmcmillen <jerrym08@me.com>
2018-02-07 17:06:05 -0500

    Only committing this to get it out of my unstaged changes list.

--- PowerUpInterative/src/org/usfirst/frc/team1002/robot/MyAutonomous.java
+++ PowerUpInterative/src/org/usfirst/frc/team1002/robot/MyAutonomous.java
@@ -1,13 +1,18 @@
 package org.usfirst.frc.team1002.robot;
 
 public class MyAutonomous {
-private double timeForward;
-MarioDrive mDrive;
+	private double timeForward;
+	private double turnDegrees;
+	MarioDrive mDrive;
+
 	public MyAutonomous() {
 		timeForward = 0;
 		mDrive = new MarioDrive();
-	}
-	public void init() {
 		
+		
+	}
+
+	public void init() {
+
 	}
 }
